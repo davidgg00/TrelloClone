502752f43d3d72a0349acbb987036d3f3ec11851
David Guisado González <51287306+davidgg00@users.noreply.github.com>
2024-12-31 13:29:11 +0100

    Add autofocus to task input field and focus on mount

--- front/src/components/TaskInput.vue
+++ front/src/components/TaskInput.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const title = ref('');
+
+const inputRef = ref<HTMLInputElement | null>(null);
+
+onMounted(() => {
+    inputRef.value?.focus();
+});
 </script>
 
 <template>
     <input type="text" v-model="title" @keydown.enter="$emit('save', title)" @keydown.esc="$emit('cancel')"
-        placeholder="New task title"
-        class="task-input border border-gray-300 rounded-lg w-full p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        autofocus />
+        placeholder="New task title" ref="inputRef"
+        class="task-input border border-gray-300 rounded-lg w-full p-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
 </template>
 
 <style scoped></style>
